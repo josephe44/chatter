6e71ec6c4942b83465537657dc68c6b408803e8f
josephe44 <josephe442@gmail.com>
2023-06-21 12:04:44 +0100

    added firebase to chatter

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKUM4WrwmzWYUy1x1wYuR-DbY6SMFpfUc",
+  authDomain: "chatter-8c56b.firebaseapp.com",
+  projectId: "chatter-8c56b",
+  storageBucket: "chatter-8c56b.appspot.com",
+  messagingSenderId: "816476001344",
+  appId: "1:816476001344:web:8763c4e4ccaf01efec3ba5",
+  measurementId: "G-QR5SS2F7M8",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const analytics = getAnalytics();
+const db = getFirestore();
+const auth = getAuth();
+const storage = getStorage();
+
+export { db, auth, storage };
