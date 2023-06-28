ecc75e6ef615ba8a091eaae914c3d7329c3f0e9b
josephe44 <josephe442@gmail.com>
2023-06-28 23:38:18 +0100

    added more content and style to create blog page

--- src/pages/dashboard/new.tsx
+++ src/pages/dashboard/new.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Fragment } from "react";
 import withLayout from "@/layouts/dasLayout";
 import HeadMeta from "@/components/head";
@@ -9,12 +9,17 @@ import {
   Container,
   Flex,
   SimpleGrid,
+  FileButton,
+  AspectRatio,
   Tabs,
   Text,
+  TextInput,
   createStyles,
   rem,
+  Group,
 } from "@mantine/core";
 import Link from "next/link";
+import { IconCamera } from "@tabler/icons-react";
 
 const useStyles = createStyles((theme) => ({
   control: {
@@ -30,10 +35,22 @@ const useStyles = createStyles((theme) => ({
       backgroundColor: theme.colors.chatter[0],
     },
   },
+
+  titleStyle: {
+    marginTop: rem(20),
+  },
+
+  imageStyle: {
+    height: "100%",
+    width: "100%",
+    objectFit: "contain",
+  },
 }));
 
 function NewBlog() {
   const { classes, theme } = useStyles();
+  const [file, setFile] = useState<File | null>(null);
+
   return (
     <Fragment>
       <HeadMeta pageName="Create New Blog" />
@@ -44,6 +61,39 @@ function NewBlog() {
 
             <Button className={classes.control}>Publish</Button>
           </Flex>
+          <Box mt={40}>
+            <Flex align="center" gap={40} w="100%">
+              {!file && (
+                <Group position="center">
+                  <FileButton onChange={setFile} accept="image/png,image/jpeg">
+                    {(props) => (
+                      <IconCamera size={48} strokeWidth={1} {...props}>
+                        Upload image
+                      </IconCamera>
+                    )}
+                  </FileButton>
+                </Group>
+              )}
+
+              <Box w="100%">
+                {file && (
+                  <AspectRatio ratio={1000 / 400}>
+                    <img
+                      src={URL.createObjectURL(file)}
+                      alt="blog image"
+                      className={classes.imageStyle}
+                    />
+                  </AspectRatio>
+                )}
+                <TextInput placeholder="Title" size="xl" variant="unstyled" />
+                <TextInput
+                  placeholder="Write a post..."
+                  size="xl"
+                  variant="unstyled"
+                />
+              </Box>
+            </Flex>
+          </Box>
         </Box>
       </Container>
     </Fragment>
